733a02e876a63b9651523afe434da1940baf53eb
emolikeme <daffa.agusta@gmail.com>
2020-01-06 13:43:09 +0700

    nyoba goride

---
+++ NYOBAGORIDE/app/src/main/java/com/faza/daffa/goridealala/PlaceAutoCompleteActivity.java
@@ -0,0 +1,14 @@
+package com.faza.daffa.goridealala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlaceAutoCompleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_auto_complete);
+    }
+}
